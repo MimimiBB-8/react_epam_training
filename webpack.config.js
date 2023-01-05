2c5fe6e4bbf52d1d64177b1cbe1347d5e1a326c9
YP <yliame14@gmail.com>
2023-01-05 17:45:39 +0200

    fix: fix bugs in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output:{
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader:'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        // new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({ template: './src/index.html' }),
-      ],
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+        
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  ],
+};
