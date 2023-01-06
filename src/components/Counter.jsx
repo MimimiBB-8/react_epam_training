3a71cb0155932f93fd77e04122e40ce4db087ecf
YP <yliame14@gmail.com>
2023-01-06 18:52:26 +0200

    rename: rename function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,17 +2,17 @@ import React, {useState} from 'react';
 
 const Counter = () => {
     const[count, setCount] = useState(0)
-    function increment(){
+    function handleIncrement(){
         setCount(count + 1)
     }
-    function decrement(){
+    function handleDecrement(){
         setCount(count - 1)
     }
     return (
         <div>
             <h1>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
         </div>
     );
 }
