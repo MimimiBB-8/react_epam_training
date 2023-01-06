8cd0bd205fc1401214b44b8eeb45acf0f7e357a4
YP <yliame14@gmail.com>
2023-01-06 18:34:37 +0200

    fix: fix typescript configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js",
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+},
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.ts$|tsx/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-        
+          loader: "ts-loader",   
       },
     ],
   },
