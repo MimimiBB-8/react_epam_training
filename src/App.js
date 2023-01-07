028d7f88d9cb52f3862c27aec6bbbe9efb3956bc
YP <yliame14@gmail.com>
2023-01-07 11:00:26 +0200

    create: create components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import SecondMethod from './components/SecondMethod';
 import ThirtMethod from './components/ThirtMethod';
 import FourthMethod from './components/FourthMethod';
 import Counter from './components/Counter';
+import Selection from './components/Selection';
 
 function App() {
   return (
     <div className="App">
-     <FirstMethod/>
+      <div>
+      <FirstMethod/>
      <SecondMethod/>
      <ThirtMethod/>
      <FourthMethod/>
+      </div>
+     <div>
      <Counter/>
+     <Selection/>
+     </div>
     </div>
   );
 }
