e0483a1a6a39f01227a485ae6e3e026ca01c60bc
YP <yliame14@gmail.com>
2023-01-11 10:29:53 +0200

    fix

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.join(__dirname, '..', 'dist'),
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', '.sass', '.css'],
   },
   module: {
     rules: [
