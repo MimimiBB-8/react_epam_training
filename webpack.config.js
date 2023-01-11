3d56580f1592d54f5bc525e60a25764d404c1133
YP <yliame14@gmail.com>
2023-01-11 12:14:13 +0200

    add dotenv and build:dev buila:prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const { merge } = require('webpack-merge');
-
 const commonConfig = require('./config/webpack.common');
+require('dotenv').config();
 
-module.exports = (env) => {
+module.exports = () => {
+  console.log(process.env.ENV);
   // eslint-disable-next-line global-require
-  const config = require(`./config/webpack.${env.env}`);// eslint-disable-line import/no-dynamic-require
+  const config = require(`./config/webpack.${process.env.ENV}`);// eslint-disable-line import/no-dynamic-require
   return merge(commonConfig, config);
 };
